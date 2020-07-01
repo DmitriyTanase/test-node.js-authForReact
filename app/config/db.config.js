67fc99074633363f9dc27fedf8c37dd7d20bcae1
dima <andreisin2001@mail.ru>
2020-07-01 17:34:46 +0300

    comment

---
+++ app/config/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    HOST: "d2207",
+    PORT: "wqrzxs1234@cluster0-37he4.mongodb.net",
+    DB: "test?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+let corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+db.mongoose
+    .connect('mongodb+srv://d2207:wqrzxs1234@cluster0-37he4.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+app.get("/", (req, res) => {
+    res.json({ message: "Enter point." });
+});
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
